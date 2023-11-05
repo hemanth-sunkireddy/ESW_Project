da62a4f1027d7152bf6bc678937023a1c54a6163
Hemanth Sunkireddy <117459441+hemanth-sunkireddy@users.noreply.github.com>
2023-11-05 16:56:22 +0530

    Update motor_codes.ino

--- Sensor_codes/motor_codes.ino
+++ Sensor_codes/motor_codes.ino
@@ -7,18 +7,18 @@ AF_DCMotor motor(3);
 */
 
 void setup() {
-    
+  Serial.begin(9600);
 }
 
 void loop() {
     motor.setSpeed(200);
     motor.run(FORWARD);
-    serial.println("---Rotating-Forward---");
+    Serial.println("---Rotating-Forward---");
     delay(1000);
     motor.run(RELEASE);
     delay(500);
     motor.run(BACKWARD);
-    serial.println("---Rotating-Backward---")
+    Serial.println("---Rotating-Backward---");
     delay(1000);
     motor.run(RELEASE);
 }
