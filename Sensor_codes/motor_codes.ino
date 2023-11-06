c1368d1f449382cc3841a06c2f4d660e9c766469
Susheel Krishna <112755744+JabadeSusheelKrishna@users.noreply.github.com>
2023-11-06 22:13:17 +0530

    Update motor_codes.ino

--- Sensor_codes/motor_codes.ino
+++ Sensor_codes/motor_codes.ino
@@ -1,24 +1,37 @@
-// Adafruit Motor Shield library - Version: Latest 
-#include <AFMotor.h>
+#include <SoftwareSerial.h>
+#include <AFMotor.h>    // DC motor Drivers
 
-AF_DCMotor motor(3);
-/*
+#define RXp2 2  // Define the pin number for the software serial RX
+#define TXp2 3  // Define the pin number for the software serial TX
 
-*/
+AF_DCMotor motor(3);  // We are providing motor to the third pin
+
+SoftwareSerial espSerial(RXp2, TXp2);  // Define a software serial port
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(4800);  // Initialize the Serial Monitor
+  espSerial.begin(4800);  // Initialize the software serial port for ESP32 communication
+  motor.setSpeed(255);
 }
 
+int oxygenLevel = 90;
+
 void loop() {
-    motor.setSpeed(200);
-    motor.run(FORWARD);
-    Serial.println("---Rotating-Forward---");
-    delay(1000);
-    motor.run(RELEASE);
-    delay(500);
-    motor.run(BACKWARD);
-    Serial.println("---Rotating-Backward---");
-    delay(1000);
-    motor.run(RELEASE);
+  if (espSerial.available()) {
+    String dataFromESP32 = espSerial.readStringUntil('\n');  // Read data from ESP32
+    Serial.println(dataFromESP32);  // Print received data to Serial Monitor
+    
+    oxygenLevel = dataFromESP32.toInt(); // Convert the received String to an integer
+    
+    if (oxygenLevel < 10) {
+      // Oxygen is low, so provide O2 for it
+      motor.run(FORWARD);
+      Serial.println("--- Low Oxygen ---");
+      Serial.println("--- Oxygen Provided Success ---");
+    } else {
+      // Turn the Motor OFF
+      motor.run(RELEASE);
+    }
+  }
+  delay(100);
 }
