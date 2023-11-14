6131e5813da6b3ff5436d2a6dadb3d0cd7dd7284
Susheel Krishna <112755744+JabadeSusheelKrishna@users.noreply.github.com>
2023-11-14 15:34:34 +0530

    Update dissolved_oxygen.h

--- sensor_codes/final/dissolved_oxygen.h
+++ sensor_codes/final/dissolved_oxygen.h
@@ -1,7 +1,7 @@
 float dissolved_oxygen(){
 
   // Code for Calculating Dissolved Oxygen 
-        float DissolvedOxygen = (70 / (77 - PH_VALUE) ) * (-0.16 * (temperature)) + 12.26;
+        float DissolvedOxygen = (70 / (77 - PH_VALUE) ) * (-0.16 * (temperature)) + 9.26;
         // (70 / (77 - pH)) * (-0.16) * temp + 9.26
         Serial.print("Dissolved Oxygen : ");
         Serial.println(DissolvedOxygen);
