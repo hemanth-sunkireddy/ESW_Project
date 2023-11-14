75d12be85f9805f151f46f85ea723b8b3b294523
Susheel Krishna <112755744+JabadeSusheelKrishna@users.noreply.github.com>
2023-11-14 15:21:42 +0530

    Update waterpump.h

--- sensor_codes/final/waterpump.h
+++ sensor_codes/final/waterpump.h
@@ -1,1 +1,21 @@
-// hello world
+// Motor A
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
+int enable1Pin = 14; 
+
+// Setting PWM properties
+const int pwmChannel = 0;
+
+void rotate(int delay_time)
+{
+  Serial.println("Motor Rotating");
+  digitalWrite(motor1Pin1, HIGH);
+  delay(delay_time);
+}
+
+void Stop(int delay_time)
+{
+  Serial.println("Motor Rotating");
+  digitalWrite(motor1Pin1, LOW);
+  delay(delay_time);
+}
